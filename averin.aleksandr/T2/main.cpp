cfad96a23e79552a32a907b744d1dce541e07532
alex <alex@MacBook-Air-alex.local>
2025-05-14 05:11:00 +0300

    main fix

--- averin.aleksandr/T2/main.cpp
+++ averin.aleksandr/T2/main.cpp
@@ -2,6 +2,7 @@
 #include <algorithm>
 #include <vector>
 #include <iterator>
+#include <limits>
 
 int main()
 {
@@ -28,45 +29,3 @@ int main()
     return 0;
 }
 
-
-
-// #include "DataStruct.hpp"
-// #include <algorithm>
-// #include <vector>
-// #include <iterator>
-// #include <iostream>
-// #include <sstream>
-// #include <string>
-
-// int main()
-// {
-//   std::vector<averin::DataStruct> data;
-//   std::string line;
-
-//   while (!std::cin.eof())
-//   {
-//     if (line.empty()) continue;
-
-//     std::istringstream lineStream(line);
-//     averin::DataStruct ds;
-
-//     if (lineStream >> ds)
-//     {
-//       data.push_back(ds);
-//     }
-//     else
-//     {
-//       continue;
-//     }
-//   }
-
-//   std::sort(data.begin(), data.end(), averin::dataStructCompare);
-
-//   std::copy(
-//     data.begin(),
-//     data.end(),
-//     std::ostream_iterator<averin::DataStruct>(std::cout, "\n")
-//   );
-
-//   return 0;
-// }
