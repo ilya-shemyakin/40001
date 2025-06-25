22afedf384a379404de209b3986c72fdc7e3828a
shabunkov2155@mail.ru <shabunkov2155@mail.ru>
2025-06-25 21:59:44 +0300

    Fix

--- shabunkov.kirill/T3/main.cpp
+++ shabunkov.kirill/T3/main.cpp
@@ -27,18 +27,18 @@ int main(int argc, char* argv[])
 
   std::vector<Polygon> polygons;
   Polygon poly;
-  while (input >> poly) {
-    polygons.push_back(poly);
-  }
 
-  if (!input.eof()) {
-    std::cerr << "ERROR: invalid input format\n";
-    return 2;
+  while (input >> poly || !input.eof()) {
+    if (input.fail()) {
+      input.clear();
+      input.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+      continue;
+    }
+    polygons.push_back(poly);
   }
 
   processCommands(polygons);
 
   return 0;
 }
-
 
